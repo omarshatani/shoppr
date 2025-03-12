f8d7d00948e7e74ab60bb7d8dd07f16d613770cf
Omar Shatani <omarsamisalem.shatani@corner.ch>
2025-03-12 16:58:43 +0100

    started adding bindings

--- app/src/main/java/com/shoppr/app/ui/overview/buy/BuyOverviewFragment.java
+++ app/src/main/java/com/shoppr/app/ui/overview/buy/BuyOverviewFragment.java
@@ -1,22 +1,22 @@
 package com.shoppr.app.ui.overview.buy;
 
-import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import com.shoppr.app.R;
+import com.shoppr.app.databinding.FragmentBuyOverviewBinding;
 
 public class BuyOverviewFragment extends Fragment {
 
 	private BuyOverviewViewModel mViewModel;
+	private FragmentBuyOverviewBinding binding;
 
 	public static BuyOverviewFragment newInstance() {
 		return new BuyOverviewFragment();
@@ -25,12 +25,16 @@ public class BuyOverviewFragment extends Fragment {
 	@Override
 	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
 													 @Nullable Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.fragment_buy_overview, container, false);
+		binding = FragmentBuyOverviewBinding.inflate(inflater, container, false);
+		return binding.getRoot();
 	}
 
 	@Override
 	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
 		mViewModel = new ViewModelProvider(this, new BuyOverviewViewModelFactory(requireActivity())).get(BuyOverviewViewModel.class);
+
+		ImageView arrowDown = binding.buySellArrowDown;
+
 	}
 }
